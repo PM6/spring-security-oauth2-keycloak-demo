4d08248e18b168b76e406dd4af56b170f9e1da21
patri <patrickmarkgraf@gmail.com>
2022-06-19 17:47:49 +0200

    webclient example

--- oauth2-client-credentials-demo/microservice-1/src/main/java/com/programming/techie/microservice1/controller/Controller1.java
+++ oauth2-client-credentials-demo/microservice-1/src/main/java/com/programming/techie/microservice1/controller/Controller1.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class Controller1 {
 
     private final RestTemplate restTemplate = new RestTemplateBuilder().build();
+    private final WebClient webClient = WebClient.builder().build();
 
     @GetMapping("/microservice1/home")
     @ResponseStatus(HttpStatus.OK)
@@ -28,6 +30,21 @@ public class Controller1 {
                 String.class);
 
         return "Hello - Message from Microservice 2 is " + response.getBody();
+    }
+
+    @GetMapping("/microservice1/home/webclient")
+    @ResponseStatus(HttpStatus.OK)
+    public String helloWebClient() {
+        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String response = webClient.get()
+                .uri("http://localhost:8084/microservice2/home")
+                .headers(header -> header.setBearerAuth(jwt.getTokenValue()))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return "Hello - Message from Microservice 2 is " + response;
     }
 
 }
